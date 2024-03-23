fb58e149c6cb0cc1268088f00f72a0324737a058
nickmaster <mchelabchi@gmail.com>
2024-03-23 07:57:33 -0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1 class="flex justify-center bg-orange-500">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	:global(html) {
+		background-color: bisque;
+	}
+</style>
