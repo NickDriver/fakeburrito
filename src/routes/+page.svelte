2c204b5de26a4a0f969091a108853d57bc63c344
nickmaster <mchelabchi@gmail.com>
2024-03-24 16:06:29 -0700

    add TS to script tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
+<script lang="ts">
+	let age: Number = 0;
+</script>
+
 <h1 class="flex justify-center bg-orange-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<style>
+<style lang="postcss">
 	:global(html) {
 		background-color: bisque;
 	}
